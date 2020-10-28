379c563c0ccaa904076617cff9e29a1cf39ccd4d
Michael Truong <michaeltruong@Michaels-MacBook-Air.local>
2020-10-28 19:41:27 -0400

    wild wild life answered

--- solutions.js
+++ solutions.js
@@ -16,20 +16,42 @@ for (let k = 0 ; k <= 200 ; k +=2) {
 // Fizz Buzz
 ////////////////////////////////
 for (let k = 1 ; k <= 100 ; k ++) {
-    if ((k % 3 === 0) && (k % 5 === 0)) {
-        console.log("FizzBuzz"); 
-    } else if (k % 5 === 0) {
-        console.log("Buzz"); 
-    } else if (k % 3 === 0) {
-        console.log("Fizz");
-    } else {
-        console.log(k); 
-    }
+    // if ((k % 3 === 0) && (k % 5 === 0)) {
+    //     console.log("FizzBuzz"); 
+    // } else if (k % 5 === 0) {
+    //     console.log("Buzz"); 
+    // } else if (k % 3 === 0) {
+    //     console.log("Fizz");
+    // } else {
+    //     console.log(k); 
+    // }
 }
 
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+// 1. Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee[2] = 5001;
+// console.log(plantee); 
+
+// 2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3] = "Gotham City";
+// console.log(wolfy); 
+
+
+// 3. Give Dart a second hometown by adding "Hawkins"
+dart[3] = ["Upside Down", "Hawkins"];
+// console.log(dart);
+
+// 4. Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+wolfy[0] = "Gameboy";
+// console.log(wolfy);
 
 
 ////////////////////////////////
