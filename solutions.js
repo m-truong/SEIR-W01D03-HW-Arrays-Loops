ed6579dab39d54f04d8b9865684be0ec08b8e7fc
Michael Truong <michaeltruong@Michaels-MacBook-Air.local>
2020-10-28 19:27:57 -0400

    fizzbuzz answered

--- solutions.js
+++ solutions.js
@@ -8,14 +8,24 @@ for (let k = 1 ; k <= 20  ; k++) {
 ////////////////////////////////
 // Get Even
 ////////////////////////////////
-for (let k = 0 ; k <=200 ; k +=2) {
-    console.log(k);
+for (let k = 0 ; k <= 200 ; k +=2) {
+    // console.log(k);
 }
 
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-
+for (let k = 1 ; k <= 100 ; k ++) {
+    if ((k % 3 === 0) && (k % 5 === 0)) {
+        console.log("FizzBuzz"); 
+    } else if (k % 5 === 0) {
+        console.log("Buzz"); 
+    } else if (k % 3 === 0) {
+        console.log("Fizz");
+    } else {
+        console.log(k); 
+    }
+}
 
 ////////////////////////////////
 // Wild Wild Life
